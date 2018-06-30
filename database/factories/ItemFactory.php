fffa6846dfd978b09ef6d34b05384023ce142ed8
Ahmed Salah <ahmed22502@gmail.com>
2018-06-30 21:35:38 +0200

    [Add] Category Model, Factory and seeder [Update] Item Factory

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Item::class, function (Faker $faker) {
     return [
-        'category_id' => rand(1,2),
+        'category_id' => rand(1,4),
         'item' => $faker->word(),
         'size' => rand(0, 1) ? 'large' : 'small',
         'pirce' => $faker->randomFloat(2, 6, 30),
